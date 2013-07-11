e51a4b2385144579fee2b55446df358cbc7de551
Ubuntu <ubuntu@ip-172-31-26-179.us-west-2.compute.internal>
2013-07-11 15:10:29 +0000

    Using function in readFileSync

--- web.js
+++ web.js
@@ -4,10 +4,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send('Hello World2!');
-    var buffer = new Buffer(100);
-    var len = buffer.write(fs.readFileSync("index.html"));
-    response.send(buffer.toString("utf-8", 0, len));
+    //  response.send('Hello World2!');
+    fs.readFileSync('index.html', function (err, data) {
+	if (err) throw err;
+	console.log(data); var buffer = new Buffer(100);
+	var len = buffer.write(data);
+	response.send(buffer.toString("utf-8", 0, len));
+
+    });
+    //var buffer = new Buffer(100);
+    //var len = buffer.write(fs.readFileSync("index.html"));
+    //response.send(buffer.toString("utf-8", 0, len));
 });
 
 var port = process.env.PORT || 5000;
