6ad1a6a3ada81eff4d212c9161e3fee5d772cf37
Ubuntu <ubuntu@ip-172-31-26-179.us-west-2.compute.internal>
2013-07-11 15:21:59 +0000

    Some more changes

--- web.js
+++ web.js
@@ -5,16 +5,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     //  response.send('Hello World2!');
-    fs.readFileSync('index.html', function (err, data) {
-	if (err) throw err;
-//	console.log(data); var buffer = new Buffer(100);
-//	var len = buffer.write(data);
-	response.send(data.toString("ascii"));
-
-    });
+    var buffer = fs.readFileSync('index.html');
     //var buffer = new Buffer(100);
     //var len = buffer.write(fs.readFileSync("index.html"));
-    //response.send(buffer.toString("utf-8", 0, len));
+    response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
